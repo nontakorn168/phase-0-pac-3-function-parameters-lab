dbadddde90015492d537ad56c04daf5c5d9a0cad
nontakorn <nontakorn_chris@hotmail.com>
2024-02-22 11:32:07 -0500

    finished

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  
+ 
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  
+  console.log(introduction("Aki")); 
+  console.log(introductionWithLanguage("Aki", "Ember.js")); 
+  console.log(introductionWithLanguageOptional("Gracie")); 
+  console.log(introductionWithLanguageOptional("Gracie", "Python")); 
